d127a6842817364ed27607139b1cfa3f390737d5
prat-bphc52 <hp.agarwal1@gmail.com>
2020-02-28 20:57:29 +0530

    Add documentation for circle

--- circle.cpp
+++ circle.cpp
@@ -5,6 +5,14 @@
 
 int center_x=250,center_y=250, cradius=7;
 
+/**
+ * @brief Plots all the points that are mirror images of the given point in 8 octants 
+ * 
+ * @param x X-coordinate (from reference 0,0) of one of the point in 8 octants to be plotted
+ * @param y Y-coordinate (from reference 0,0) of one of the point in 8 octants to be plotted
+ * @param centerX X-coordinate of center
+ * @param centerY Y-coordinate of center
+ */
 void Circle::getAllPoints(int x,int y, int centerX, int centerY){
     Helper::plotPoint(x+centerX, y+centerY);
     Helper::plotPoint(-x+centerX, y+centerY);
@@ -16,6 +24,14 @@ void Circle::getAllPoints(int x,int y, int centerX, int centerY){
     Helper::plotPoint(-y+centerX, -x+centerY);
 }
 
+/**
+ * @brief Draws a circle by determining and plotting each pixel based on Bresenham's Circle Drawing algo
+ * using OpenGL
+ * 
+ * @param centerX X-Coordinate of center
+ * @param centerY Y-Coordinate of center
+ * @param radius Radius of the circle
+ */
 void Circle::drawCircle(int centerX, int centerY, int radius){
     int x=0,y=radius;
     int decision = 1 - radius;
