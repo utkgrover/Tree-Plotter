d4969bf665fda1547e86f81fcbfc0b39ab1007a1
prat-bphc52 <hp.agarwal1@gmail.com>
2020-02-28 19:22:26 +0530

    Add documentation for tree class

--- tree.cpp
+++ tree.cpp
@@ -10,9 +10,25 @@ using namespace std;
 
 const int WINDOW_WIDTH = 1000;
 const int WINDOW_HEIGHT = 1000;
+/**
+ * @brief The distance to be maintained between each level of the binary Tree
+ * 
+ */
 const int DISTANCE_LEVEL = 100;
+/**
+ * @brief Top margin to be left on the screen before drawing the root of the Tree
+ * 
+ */
 const int TOP_MARGIN = 50;
+/**
+ * @brief The minimum separation between two nodes at the same level
+ * 
+ */
 const int MINSEP = 100;
+/**
+ * @brief Radius to be used while drawing the circles representing a node in the binary tree
+ * 
+ */
 const int RADIUS = 20;
 
 static node *treeRoot;
@@ -20,10 +36,14 @@ static node *treeRoot;
 void setup(node *T, int LEVEL, extreme *RMOST, extreme *LMOST);
 void convert(node *T, int XPOS);
 
-void inorder(node *root)
-{
-	if (root == NULL)
-		return;
+/**
+ * @brief 
+ * 
+ * @param root 
+ */
+void inorder(node *root){
+	if (root == NULL) return;
+
 	inorder(root->LLINK);
 	printf("Node: %d X-coord %d Ycoord %d\n", root->val, root->XCOORD, root->YCOORD);
 	inorder(root->RLINK);
@@ -36,8 +56,7 @@ string s1 = "yyyyynnyynnyynnyynnynnynnynnynnyynnyynnyynnyyyyynnynnynnynnynn";
 string s4 = "ynynynynynnynn";
 int string_pos = 0;
 
-void func(node *root)
-{
+void func(node *root){
 	root->val = cur_val++;
 	root->XCOORD = 0;
 	root->YCOORD = 0;
@@ -45,41 +64,40 @@ void func(node *root)
 
 	char c = s1[string_pos++];
 
-	if (c == 'y')
-	{
+	if (c == 'y'){
 		root->LLINK = (node *)malloc(sizeof(node));
 		func(root->LLINK);
 	}
-	else
-		root->LLINK = NULL;
-		
+	else root->LLINK = NULL;
+	
 	c = s1[string_pos++];
-	if (c == 'y')
-	{
+	if (c == 'y'){
 		root->RLINK = (node *)malloc(sizeof(node));
 		func(root->RLINK);
 	}
-	else
-		root->RLINK = NULL;
+	else root->RLINK = NULL;
 }
 
-node *getSampleTree()
-{
+node *getSampleTree(){
 	node *root = (node *)malloc(sizeof(node));
 	func(root);
 	return root;
 }
 
-void drawTree(node *root)
-{
+/**
+ * @brief This function draws a binary Tree on a window using OpenGL based on Tidier Tree Drawing 
+ * ensuring that there is no collision between any two nodes of the tree and minimizing the 
+ * overall width of the tree
+ * 
+ * @param root pointer to the root of the tree which has to be plotted
+ */
+void drawTree(node *root){
 	Circle::drawCircle(root->XCOORD, root->YCOORD, RADIUS);
-	if (root->LLINK != NULL)
-	{
+	if (root->LLINK != NULL){
 		Line::drawLine(root->XCOORD, root->YCOORD, root->LLINK->XCOORD, root->LLINK->YCOORD, RADIUS + 2);
 		drawTree(root->LLINK);
 	}
-	if (root->RLINK != NULL)
-	{
+	if (root->RLINK != NULL){
 		Line::drawLine(root->XCOORD, root->YCOORD, root->RLINK->XCOORD, root->RLINK->YCOORD, RADIUS + 2);
 		drawTree(root->RLINK);
 	}
@@ -90,7 +108,13 @@ void draw(){
 	glFlush();
 }
 
-int initTree(int argc, char **argv){
+/**
+ * @brief 
+ * This function is used to take input from the user, generate a tree and display it on a window using OpenGL
+ * @param argc The total number of arguments
+ * @param argv The actual arguments to be passed to the function
+ */
+void Tree::initTree(int argc, char **argv){
 	extreme *a = (extreme *)malloc(sizeof(extreme));
 	extreme *b = (extreme *)malloc(sizeof(extreme));
 
@@ -106,6 +130,12 @@ int initTree(int argc, char **argv){
 	glutMainLoop();
 }
 
+/**
+ * @brief A recursive function to determine the absolute coordinates of each node using the offset calculated
+ * 
+ * @param T Pointer to the root of the subtree whose absolute coordinate is to be determined
+ * @param XPOS The X-Coordinate that has to be assigned to this node passed as 1st parameter
+ */
 void convert(node *T, int XPOS){
 	if (T != NULL){
 		T->XCOORD = XPOS;
@@ -119,6 +149,16 @@ void convert(node *T, int XPOS){
 	}
 }
 
+/**
+ * @brief A recursive function to determine the offsets for each sub tree of the given root so that
+ * they can be drawn without any collision at any level. This uses the Tidier Algorithm to draw binary trees
+ * ensuring minimum overall width of the tree
+ * 
+ * @param T Pointer to the root node of the tree whose child offsets are to be determined
+ * @param LEVEL Current level of this node
+ * @param RMOST Pointer to right most nodes used to determine offset efficiently
+ * @param LMOST Pointer to right most nodes used to determine offset efficiently
+ */
 void setup(node *T, int LEVEL, extreme *RMOST, extreme *LMOST){
 	int CURSEP;			  // SEPARATION ON CURRENT LEVEL
 	int ROOTSEP;		  //CURRENT SEPARATION AT NODE T
@@ -132,13 +172,11 @@ void setup(node *T, int LEVEL, extreme *RMOST, extreme *LMOST){
 	RR = (extreme *)malloc(sizeof(extreme));
 	RL = (extreme *)malloc(sizeof(extreme));
 
-	if (T == NULL)
-	{ // AVOID SELECTING AS EXTREME
+	if (T == NULL){ // AVOID SELECTING AS EXTREME
 		LMOST->LEV = -1;
 		RMOST->LEV = -1;
 	}
-	else
-	{
+	else{
 		T->YCOORD = WINDOW_HEIGHT - TOP_MARGIN - LEVEL * DISTANCE_LEVEL;
 		L = T->LLINK;				 //FOLLOWS CONTOUR OF LEFT SUBTREE
 		R = T->RLINK;				 // FOLLOWS CONTOUR OP RIGNT SUBTREE
